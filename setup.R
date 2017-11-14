a2a20004c48b719e2b42d896d88dfdc1252f0d8d
Robyn <robs.steenekamp@gmail.com>
2017-11-14 16:49:47 +0200

    packrat
    commit to master so can pu

--- setup.R
+++ setup.R
@@ -1,2 +1,9 @@
+
+#load packages
+#if(!require("devtools")) install.packages("devtools",depend=T)
+devtools::document()
+#install.packages(pkg,depend=T,repos=getOption("repos"),type = "source",lib=)
+
 #run main.R script
 #render README.md
+
